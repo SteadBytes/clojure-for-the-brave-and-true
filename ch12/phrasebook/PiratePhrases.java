f02368ec81340838f5f05657a525a6a66e07da94
Ben Steadman <steadmanben1@gmail.com>
2019-07-21 07:23:42 +0100

    ch12 Java examples

---
+++ ch12/phrasebook/PiratePhrases.java
@@ -0,0 +1,8 @@
+// 1 public class per file & file name must match class name
+public class PiratePhrases
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Shiver me timbers!");
+    }
+}
---
+++ ch12/phrasebook/pirate_phrases/Farewells.java
@@ -0,0 +1,9 @@
+package pirate_phrases;
+
+public class Farewells
+{
+    public static void goodbye()
+    {
+        System.out.println("A fair turn of the tide ter ye that, ye magnificent sea friend!!");
+    }
+}
